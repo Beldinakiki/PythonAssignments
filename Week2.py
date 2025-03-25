5326e13578fad4e5ecc8953c8a569dbc8a6e95bf
Beldinakiki <kinyuabeldina@gmail.com>
2025-03-25 11:51:55 +0300

    Week 2 Assignment

---
+++ Week2.py
@@ -0,0 +1,16 @@
+#Create empty list
+my_list=[]
+#append elements to list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#extend list with another list
+my_list.extend([50,60,70])
+#remove last item from list
+my_list.pop()
+#sort list in ascending order
+my_list.sort()
+print(my_list)
+#Print 30
+print(my_list.index(30))
